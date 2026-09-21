4f85473e2650b8c7f418d58ac4d6054c0e3cacee
nekomaron <holmes.boyle@gmail.com>
2026-09-21 20:01:50 +0900

    Hyperlandの最小構成を追加、起動・ターミナル(kitty)の起動を確認

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,1 +1,18 @@
-{ ... }: { }
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.kitty
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod, Q, killactive"
+      ];
+    };
+  };
+}
