0a7a81b87ce4d04b2e4383aebbefb1aeb99e1dfe
nekomaron <holmes.boyle@gmail.com>
2026-09-21 20:27:56 +0900

    greetd+regreetを追加、GTK4用グラフィック環境解決のためcageをラップ

--- modules/system/display/default.nix
+++ modules/system/display/default.nix
@@ -7,4 +7,18 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/log/regreet 0755 greeter greeter -"
+  ];
 }
