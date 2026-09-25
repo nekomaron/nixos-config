e823cea4f2d5a38311b031102dc8013d9ab5e4c9
nekomaron <holmes.boyle@gmail.com>
2026-09-25 11:58:33 +0900

    Git設定を追加(programs.git.settings, delta連携)、非推奨オプションを新形式に対応

--- modules/home/dev/default.nix
+++ modules/home/dev/default.nix
@@ -1,1 +1,27 @@
-{ ... }: { }
+{ pkgs, lib, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    # TODO: 実際のGitHubアカウント情報に差し替える
+    # userName = "あなたの名前";
+    # userEmail = "your-email@example.com";
+
+    settings = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      core.editor = "nvim";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        tree = "log --graph --oneline --all --decorate";
+      };
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+}
